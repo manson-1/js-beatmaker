e59871ded5a3dad9e4cea09b50dc8136b46b6766
Manuel <manuel.sonnleithner@gmail.com>
2021-02-28 19:26:04 +0100

    playing sounds in js file

--- app.js
+++ app.js
@@ -15,7 +15,27 @@ class DrumKit{
 
     repeat(){
         let step = this.index % 8; // if we are on position 8 (index = 8), step will go to 0
-        const activeBar = document.querySelector(`.b${step}`); // need to use backticks for the string template literal to work. we grab all 3 tracks on the current position with this selector
+        const activeBar = document.querySelectorAll(`.b${step}`); // need to use backticks for the string template literal to work. we grab all 3 tracks on the current position with this selector
+        
+        activeBar.forEach((bar) => {
+            bar.style.animation = `playTrack 0.3s alternate ease-in-out 2` // add the animation to each bar-row of pads. alternate = run anim back and forth. Run anim 2 times, to run 1x forth and 1x back
+            
+            if(bar.classList.contains("active")){ // if the pad is active (clicked)
+                if(bar.classList.contains("kick-pad")){ // if pad is kick-pad, play sound
+                    this.kickAudio.currentTime = 0; // set back play-time so to avoid that the sound is not played because the last sound is not finished yet
+                    this.kickAudio.play();
+                }
+                if(bar.classList.contains("snare-pad")){ // if pad is kick-pad, play sound
+                    this.snareAudio.currentTime = 0; // set back play-time so to avoid that the sound is not played because the last sound is not finished yet
+                    this.snareAudio.play();
+                }
+                if(bar.classList.contains("hihat-pad")){ // if pad is kick-pad, play sound
+                    this.hihatAudio.currentTime = 0; // set back play-time so to avoid that the sound is not played because the last sound is not finished yet
+                    this.hihatAudio.play();
+                }
+            }
+        })
+
         this.index++; // increment index by 1 to move to the next bar
         console.log(step);
     }
@@ -32,6 +52,9 @@ const drumKit = new DrumKit(); // initialize
 
 drumKit.pads.forEach(pad => {
     pad.addEventListener("click", drumKit.activePad); // on click on a pad, it should toggle active/deactive mode
+    pad.addEventListener("animationend", () => {
+        pad.style.animation = ""; // after the animation is finished for each pad, reset the animation to nothing, so the next time when the pad is active, its again gets the animation added and therefore plays back the animation
+    });
 });
 
 drumKit.playBtn.addEventListener("click", () => drumKit.start()); // start the drumkit on click on play button. Arrow function is needed, so the correct "this" is used in the methods
